82261923379e3a51d01bd5bfba1f841c73f450d5
ricky-blip <ricky.rinaldy77@gmail.com>
2025-07-20 20:16:39 +0700

    first commit

---
+++ src/main/java/com/ricky/ricky_rest_api/RickyRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ricky.ricky_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickyRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickyRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricky/ricky_rest_api/RickyRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricky.ricky_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickyRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
